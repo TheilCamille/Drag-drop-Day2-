09de053bea413c3dfc83f18005f7b383690b35ce
TheilCamille <camille@theil.fr>
2016-10-26 16:57:06 +0200

    not finish

--- js/horscontexte.js
+++ js/horscontexte.js
@@ -1,22 +1,38 @@
 $( function() {
     var x = 0;
+    $('.hammer').hide();
+
+
     $(".ui-widget-content").draggable({
-        revert: function() {
+        helper: function() {
             return jQuery(this).clone().appendTo('body');
-        },
+        }
     });
     $( "#pig" ).droppable({
+
         drop: function(event, ui) {
             if(ui.draggable.is("#coin4")){
                 $(this).addClass("ui-state-highlight").find("p").html("1€ ? C'est toujours ça :)");
                 x++;
                 $('#count').text(x);
+                if (x > 40) {
+                    $('.hammer').show();
+                  
+                }
+
+
             }
             else if(ui.draggable.is("#coin3")){
                 $(this).addClass("ui-state-hightlight").find("p").html("2€ ? Merci !");
                 x++;
                 x++;
                 $('#count').text(x);
+                if (x > 40) {
+                    $('.hammer').show();
+
+                }
+
+
             }
 
             else if(ui.draggable.is("#coin1")){
@@ -27,15 +43,28 @@ $( function() {
                 x++;
                 x++;
                 $('#count').text(x);
+                if (x > 40) {
+                    $('.hammer').show();
+                }
+
             }
             else if(ui.draggable.is("#coin2")){
                 alert("Je n'accepte pas les boutons !");
                 ui.draggable.draggable('option','revert', true);
+                if (x > 40) {
+                    $('.hammer').show();
+
+                }
             }
+
+
+
+
         }
-    });
+
 
 });
-
-
-
+    $('.hammer').click(function(){
+        $('#pig').css('background-image','url ("/assets/img/cochon.png")');
+    });
+});
